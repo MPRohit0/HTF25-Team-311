a76e5190b7ba3e1d8b3d6bc70e4f0b425fa7b9cd
MPRohit00 <rohit.gopu00@gmail.com>
2025-10-26 02:44:28 +0530

    fix mistake

--- learning_lol/models.py
+++
@@ -1,9 +0,0 @@
-# we use pydantate for validation we use pydantic
-from pydantic import BaseModel
-
-class Products(BaseModel):
-    id: int
-    name: str
-    description: str
-    price: float
-    quantity: int
